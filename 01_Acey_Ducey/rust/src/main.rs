7fca6e32972e6f41f5f6f84f8e0ab57a96903c1b
Allyn L. Bottorff <abottorff@gmail.com>
2023-07-08 13:55:28 -0500

    working guess system

--- 01_Acey_Ducey/rust/src/main.rs
+++ 01_Acey_Ducey/rust/src/main.rs
@@ -1,139 +1,60 @@
-use rand::{prelude::ThreadRng, Rng};
-use std::{fmt, io, mem};
+use rand::Rng;
 
-#[derive(PartialEq, Eq, PartialOrd, Ord)]
-struct Card(u8);
-
-impl Card {
-    fn new_random(rng: &mut ThreadRng) -> Card {
-        Card(rng.gen_range(2..15))
-    }
-}
-
-impl fmt::Display for Card {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self.0 {
-                11 => String::from("JACK"),
-                12 => String::from("QUEEN"),
-                13 => String::from("KING"),
-                14 => String::from("ACE"),
-                otherwise => otherwise.to_string(),
-            }
-        )
-    }
-}
-
-struct CardsPool(Card, Card, Card);
-
-impl CardsPool {
-    fn new() -> CardsPool {
-        let mut rng = rand::thread_rng();
-        let mut first = Card::new_random(&mut rng);
-        let mut second = Card::new_random(&mut rng);
-        let third = Card::new_random(&mut rng);
-
-        if first > second {
-            mem::swap(&mut first, &mut second);
-        }
-
-        CardsPool(first, second, third)
-    }
-
-    fn is_in_win_range(&self) -> bool {
-        self.0 <= self.2 && self.2 <= self.1
-    }
-}
+const DECK: [&str; 13] = [
+    "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A",
+];
 
 fn main() {
-    hello();
-    // user start bank
-    let mut user_bank: u16 = 100;
+    println!("Welcome to Acey Ducey!");
+    println!("How to play:");
+    println!("The dealer deals out two cards face up. You have the option to bet");
+    println!("or to pass based on whether you feel the next card will have a value");
+    println!("between the two dealt cards. Aces are high. If you win, you earn double");
+    println!("your bet.");
+    println!("You start with $100.");
+    println!("Enter 'p' to pass or 'q' to quit.");
+    println!("-----\n");
 
-    loop {
-        println!("YOU NOW HAVE {} DOLLARS.", &mut user_bank);
-        println!("\nHERE ARE YOUR NEXT TWO CARDS:");
-        // get new random cards
-        let cards = CardsPool::new();
+    let mut wallet = 100; // This is the amount of money the player currently has.
+    println!("Current wallet: ${}", wallet);
+    play(&mut wallet);
+}
 
-        println!("{}", cards.0);
-        println!("{}", cards.1);
+fn play(wallet: &mut i32) {
+    // Get a deck of cards, draw two cards and remove them from the deck.
+    let mut cards: Vec<&str> = Vec::from(DECK);
+    let dealer_index = rand::thread_rng().gen_range(0..cards.len());
+    let dealer_card_1 = cards.remove(dealer_index);
+    let dealer_index = rand::thread_rng().gen_range(0..cards.len());
+    let dealer_card_2 = cards.remove(dealer_index);
+    let player_index = rand::thread_rng().gen_range(0..cards.len());
+    let player_card = cards.remove(player_index);
 
-        let user_bet: u16 = get_bet(user_bank);
-
-        if user_bet == 0 {
-            println!("CHICKEN!!!\n");
-            continue;
-        } else {
-            println!("THANK YOU! YOUR BET IS {} DOLLARS.", user_bet);
+    // loop back over the reference deck to check for the values of the cards
+    let mut dealer_card_1_val = 0;
+    let mut dealer_card_2_val = 0;
+    let mut player_card_val = 0;
+    for (i, v) in DECK.iter().enumerate() {
+        if *v == dealer_card_1 {
+            dealer_card_1_val = i;
         }
-
-        println!("\nTHE THIRD CARD IS:");
-        println!("{}", cards.2);
-
-        if cards.is_in_win_range() {
-            println!("\nYOU WIN!!!");
-            user_bank += user_bet;
-        } else {
-            println!("\nSORRY, YOU LOSE");
-            user_bank -= user_bet;
+        if *v == dealer_card_2 {
+            dealer_card_2_val = i;
         }
-
-        if user_bank == 0 {
-            println!("\nSORRY, FRIEND, BUT YOU BLEW YOUR WAD.");
-            println!("\nTRY AGAIN? (YES OR NO)");
-            let mut input = String::new();
-            io::stdin().read_line(&mut input).expect("Incorrect input");
-
-            if input.trim().to_lowercase() == "yes" {
-                user_bank = 100;
-                println!();
-            } else {
-                println!("\nO.K., HOPE YOU HAD FUN!");
-                break;
-            }
+        if *v == player_card {
+            player_card_val = i;
         }
     }
-}
 
-fn hello() {
-    println!("         🂡  ACEY DUCEY CARD GAME 🂱");
-    println!("CREATIVE COMPUTING - MORRISTOWN, NEW JERSEY");
-    println!("ACEY-DUCEY IS PLAYED IN THE FOLLOWING MANNER");
-    println!("THE DEALER (COMPUTER) DEALS TWO CARDS FACE UP");
-    println!("YOU HAVE AN OPTION TO BET OR NOT BET DEPENDING");
-    println!("ON WHETHER OR NOT YOU FEEL THE CARD WILL HAVE");
-    println!("A VALUE BETWEEN THE FIRST TWO.");
-    println!("IF YOU DO NOT WANT TO BET IN A ROUND, ENTER 0");
-    println!("\n\n");
-}
-
-fn get_bet(user_bank: u16) -> u16 {
-    loop {
-        println!("\nWHAT IS YOUR BET? ENTER 0 IF YOU DON'T WANT TO BET (CTRL+C TO EXIT)");
-        let bet: u16;
-        let mut input = String::new();
-
-        io::stdin()
-            .read_line(&mut input)
-            .expect("CANNOT READ INPUT!");
-
-        match input.trim().parse::<u16>() {
-            Ok(i) => bet = i,
-            Err(e) => {
-                println!("CHECK YOUR INPUT! {}!", e.to_string().to_uppercase());
-                continue;
-            }
-        };
-
-        match bet {
-            bet if bet <= user_bank => return bet,
-            _ => {
-                println!("\nSORRY, MY FRIEND, BUT YOU BET TOO MUCH.");
-                println!("YOU HAVE ONLY {} DOLLARS TO BET.", user_bank);
-            }
-        };
+    let dealer_cards = match dealer_card_1_val < dealer_card_2_val {
+        true => format!("{}, {}", dealer_card_1, dealer_card_2),
+        false => format!("{}, {}", dealer_card_2, dealer_card_1),
+    };
+    println!("Dealer cards: {}", dealer_cards);
+    println!("Player card: {}", player_card);
+    if (dealer_card_1_val < player_card_val && player_card_val < dealer_card_2_val)
+        || (dealer_card_2_val < player_card_val && player_card_val < dealer_card_1_val)
+    {
+        println!("Player wins!");
     }
 }
